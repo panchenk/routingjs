ac63585c3d531da993a0f8d08d6f0f97c4c23ec6
panchenk <panchenk@email.sc.edu>
2017-04-28 11:05:49 -0400

    .

---
+++ js/algorithm.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+    
+    $( "#onSimulate" ).on( "click", function( event ) 
+    {
+        event.preventDefault();
+        console.log("Running the simulation");
+        
+
+
+
+
+
+
+    });
+
+    console.log("hello world!");
+});
